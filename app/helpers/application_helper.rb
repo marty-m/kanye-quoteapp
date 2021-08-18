bac959c3b59afab4fd8dc6bff845632f1397e08e
Martynas Miliauskas <milixmar@gmail.com>
2021-08-18 20:09:45 +0200

    Dimmed background images manually, removed one low quality background. App appears to be finished and ready to push to heroku

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,2 @@
 module ApplicationHelper
-  def randomized_background_image
-    images = ["app/assets/kanye1.jpg", "app/assets/kanye2.jpg", "app/assets/kanye3.jpg", "app/assets/kanye4.jpg", "app/assets/kanye5.jpg", "app/assets/kanye6.jpg", "app/assets/kanye7.jpg", "app/assets/kanye8.jpg", "app/assets/kanye9.jpg", "app/assets/kanye10.jpg", "app/assets/kanye11.jpg", "app/assets/kanye12.jpg"]
-    images[rand(images.size)]
-  end
 end
